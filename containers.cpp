14aebd79d64663bc6c7a721d5bd1e8df546b5694
Kálley Wilkerson <yellakkalley@hotmail.com>
2018-11-16 14:08:41 -0200

    Funções de incluir, containers de acomodação e de usuário

---
+++ containers.cpp
@@ -0,0 +1,35 @@
+#include "containers.hpp"
+
+bool ContainerAcomodacao::incluir(Acomodacao acomodacao)
+{
+    string identificador = acomodacao.getIdentificador().getIdentificador();
+    list<Acomodacao>::iterator elemento;
+
+    for(elemento = container.begin(); elemento != container.end(); elemento++)
+    {
+        if(elemento->getIdentificador().getIdentificador() == identificador)
+        {
+            return false;
+        }
+    }
+
+    container.push_back(acomodacao);
+    return true;
+}
+
+bool ContainerUsuario::incluir(Usuario usuario)
+{
+    string identificador = usuario.getIdentificador().getIdentificador();
+    list<Usuario>::iterator elemento;
+
+    for(elemento = container.begin(); elemento != container.end(); elemento++)
+    {
+        if(elemento->getIdentificador().getIdentificador() == identificador)
+        {
+            return false;
+        }
+    }
+
+    container.push_back(usuario);
+    return true;
+}
--- containers.hpp
+++ containers.hpp
@@ -1,6 +1,29 @@
 #ifndef CONTAINERS_HPP_INCLUDED
 #define CONTAINERS_HPP_INCLUDED
 
+#include "dominios.hpp"
+#include "entidades.hpp"
 
+#include <list>
+
+class ContainerAcomodacao
+{
+private:
+    list<Acomodacao> container;
+
+public:
+    bool incluir(Acomodacao acomodacao);
+    bool remover(Identificador acomodacao);
+};
+
+class ContainerUsuario
+{
+private:
+    list<Usuario> container;
+
+public:
+    bool incluir(Usuario usuario);
+    bool remover(Identificador idUsuario);
+};
 
 #endif // CONTAINERS_HPP_INCLUDED
